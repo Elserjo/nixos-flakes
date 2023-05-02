c1ab5d9d299229cde1ade6d028c86d36ded3c8eb
Serg <reznov90210@gmail.com>
2023-05-02 23:14:29 +0300

    I forget add this files to prev. commit

--- scripts/default.nix
+++ scripts/default.nix
@@ -1,6 +1,13 @@
-{ cfg, pkgs, ... }:
+{ cfg, pkgs, lib, ... }:
 
-{
-    my-name = "hello-script";
-    my-script = (pkgs.writeScriptBin my-name (builtins.readfile ./hello.sh));
-} 
+#https://github.com/NixOS/nixpkgs/blob/dade7540afee3578f7a4b98a39af42052cbc4a85/pkgs/build-support/trivial-builders.nix#L228-L253
+
+let
+  hello-script = pkgs.writeTextFile {
+    name = "hello-script";
+    executable = true;
+    destination = "/bin/hello-script";
+    text = builtins.readFile ./hello.sh;
+  };
+
+in { environment.systemPackages = with pkgs; [ hello-script ]; }
