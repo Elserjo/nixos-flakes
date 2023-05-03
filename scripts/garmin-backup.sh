240cebaa5b4576e646847249bb9cf6d1a2e83075
Serg <reznov90210@gmail.com>
2023-05-03 21:02:38 +0300

    Added garmin backup bash script

--- scripts/garmin-backup.sh
+++ scripts/garmin-backup.sh
@@ -1,52 +1,10 @@
-#!/bin/bash
-
+#!/usr/bin/env bash
 
 on_error() 
 {
      _MSG_=$1
      echo "$_MSG_"
      exit 1
-}
-
-get_distro()
-{
-    local distro=$(grep -woP "ID=\K\w+" /etc/os-release)
-    local backupName="$(cat ${BACKUP_NAME_PATH})"
-     
-     case $distro in
-     "gentoo") 
-          #BACKUP_FOLDER="$MPOINT_LINUX/root_gentoo";
-          LOG_NAME="${backupName}"
-          ;;
-     "arch") 
-          #BACKUP_FOLDER="$MPOINT_LINUX/root"
-          LOG_NAME=""
-          ;;
-     *) 
-          on_error "Unknown distro"
-          ;;
-     esac
-}
-
-set_backup_name()
-{
-    if [[ ! -f "${BACKUP_NAME_PATH}" ]]; then
-        on_error "Backup name is empty"
-    fi
-    
-    local backupName="$(cat ${BACKUP_NAME_PATH})"
-
-    if [[ -z "${backupName}" ]]; then
-        on_error "Backup config file is empty"
-    fi
-
-    local ans=""
-    read -p "Backup name is: ${backupName}. Continue ? (y/N): " ans
-    if [[ ! "${ans}" =~ (y|Y) ]]; then
-        on_error "Exiting..."
-    fi
-
-    BACKUP_FOLDER="${MPOINT_LINUX}/${backupName}"
 }
 
 do_test()
@@ -127,68 +85,4 @@ garmin()
     #echo "$_COPY_STR"
 }
 
-
-backup_data() 
-{
-    if [[ "$EUID" -ne 0 ]]; then 
-    	on_error "Should be run as root"
-    fi
-    
-    BACKUP_FOLDER="" #global variable for backup path
-    BACKUP_NAME_PATH=""
-    MPOINT_WINDOWS="$(df -h | grep -i "Windows" | awk '{print $NF}')"
-    MPOINT_MEDIA="$(df -h | grep "/home/serg/Media" | awk '{print $NF}')"
-    MPOINT_LINUX="$(df -h | grep -i "Linux" | awk '{print $NF}')"
-    BACKUP_NAME_PATH="${MPOINT_LINUX}/backupConfig"
-
-    do_test "rsync"
-    do_test "awk"
-    do_test "grep"
-    do_test "df"
-
-    check_mount "${MPOINT_MEDIA}" "Media"
-    check_mount "${MPOINT_LINUX}" "Linux"
-    check_mount "${MPOINT_WINDOWS}" "Windows"
-
-    local PASS_PATH="${MPOINT_MEDIA}/syncthing/Пароли.kdbx"
-
-    set_backup_name #set output backup folder on external hdd
-    get_distro
-
-    #we want to skip windows *.LNK files for better rsync copy
-    rsync -aAXHv --delete --progress \
-        --exclude "/dev/*" \
-        --exclude "/proc/*" \
-        --exclude "/sys/*" \
-        --exclude "/tmp/*" \
-        --exclude "/run/*" \
-        --exclude "/mnt/*" \
-        --exclude "/media/*" \
-        --exclude "/lost+found" \
-        --exclude "Backup/*" \
-        --exclude "Media/*" \
-        --exclude "Work/*" \
-        --exclude "/home/*/.cache/*" \
-        --exclude "/home/*/.local/share/TelegramDesktop/tdata/*" \
-        --exclude "Soulseek Downloads/*" \
-        --exclude "*.LNK" \
-          / "$BACKUP_FOLDER" \
-        && writeLog "$LOG_NAME" "$MPOINT_LINUX"
-
-    cp -v "${PASS_PATH}" "${MPOINT_WINDOWS}"
-
-    umount "${MPOINT_LINUX}"
-    umount "${MPOINT_WINDOWS}"
-}
-
-while getopts "gl" opt
-    do
-        case $opt in
-        g) garmin
-           ;;
-        l) backup_data
-           ;;
-        *) echo "Nothing to do"
-           ;;
-        esac
-    done
+garmin
