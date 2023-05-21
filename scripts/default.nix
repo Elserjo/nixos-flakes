cf80ee4e908a0151134ee8aa1f7e8923ab1fa764
Serg <reznov90210@gmail.com>
2023-05-21 19:58:29 +0300

    Now Garmin will always be mounted to /media

--- scripts/default.nix
+++ scripts/default.nix
@@ -7,23 +7,26 @@ let
   garmin-backup = pkgs.writeShellApplication {
     name = "garmin-backup";
     text = builtins.readFile ./garmin-backup.sh;
-    runtimeInputs = [ pkgs.libnotify pkgs.rsync pkgs.coreutils pkgs.util-linux ];
+    runtimeInputs =
+      [ pkgs.libnotify pkgs.rsync pkgs.coreutils pkgs.util-linux ];
   };
 
 in {
   systemd.user.services."${garminService}" = {
     description = "Backup garmin activities when Garmin 830 is connected";
-    requires = [ "run-media-serg-GARMIN.mount" ];
-    after = [ "run-media-serg-GARMIN.mount" ];
-    wantedBy = [ "run-media-serg-GARMIN.mount" ];
+    requires = [ "media-GARMIN.mount" ];
+    after = [ "media-GARMIN.mount" ];
+    wantedBy = [ "media-GARMIN.mount" ];
     serviceConfig = {
       ExecStart = "${garmin-backup}/bin/garmin-backup";
       Type = "forking";
     };
   };
+  systemd.tmpfiles.rules = [ "D /media 0755 root root 0 -" ];
+  #Prevent automount Garmin 830 to /run/$USER/...
+  #use /media instead
   services.udev.extraRules = ''
-        ACTION=="change", SUBSYSTEM=="block",
-        ENV{ID_VENDOR_ID}=="091e", ENV{ID_MODEL_ID}=="2c32", ENV{ID_SERIAL}=="Garmin_GARMIN_Flash-0:0",
-    ENV{SYSTEMD_WANTS}=="${garminService}.service"
+        ACTION=="change", SUBSYSTEM=="block", ENV{ID_VENDOR_ID}=="091e", ENV{ID_MODEL_ID}=="2c32",
+    ENV{ID_SERIAL}=="Garmin_GARMIN_Flash-0:0", ENV{UDISKS_FILESYSTEM_SHARED}="1"
   '';
 }
