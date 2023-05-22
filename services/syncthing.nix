3e3184805e3d338c6d95492ba4c6f26dff49eadf
Serg <reznov90210@gmail.com>
2023-05-22 23:48:42 +0300

    Declarative syncthing configuration

--- services/syncthing.nix
+++ services/syncthing.nix
@@ -4,5 +4,25 @@
   services.syncthing = {
     enable = true;
     user = "serg";
+    configDir = "/home/serg/.config/syncthing";
+    overrideDevices = true;
+    overrideFolders = true;
+    devices = {
+      "synology" = {
+        id = "IQBNIEA-MZQZGBU-YM2KVJE-B6D3QAV-UIXVCSU-OD5MIRY-E3UMC25-4ROKIAF";
+      };
+    };
+    folders."syncthing" = {
+      path = "~/Media/syncthing";
+      devices = [ "synology" ];
+      versioning = {
+        type = "simple";
+        params = {
+          keep = "5";
+          cleanoutDays = "30";
+          cleanInterval = "3600";
+        };
+      };
+    };
   };
 }
