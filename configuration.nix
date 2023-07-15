6bd5a79efdb1efaedba07185a452dcc432bc151d
Serg <reznov90210@gmail.com>
2023-07-15 18:13:38 +0300

    Add android-tools package

--- configuration.nix
+++ configuration.nix
@@ -55,13 +55,14 @@
   # Define a user account. Don't forget to set a password with ‘passwd’.
   users.users.serg = {
     isNormalUser = true;
-    extraGroups = [ "wheel" "audio" "video" ]; # Enable ‘sudo’ for the user.
+    extraGroups = [ "wheel" "audio" "video" "adbusers"]; # Enable ‘sudo’ for the user.
 
     packages = with pkgs; [
       nixfmt # Nix code formatter
     ];
   };
 
+  programs.adb.enable = true;
   fonts.fonts = with pkgs; [ terminus_font noto-fonts noto-fonts-cjk-sans ];
 
   fonts.enableDefaultFonts = true;
