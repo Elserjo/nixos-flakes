df3c8796d81bb437ae881c8737c8f0b36115dfa5
Serg <reznov90210@gmail.com>
2023-07-20 21:39:26 +0300

    Remove lm_sensors package

--- configuration.nix
+++ configuration.nix
@@ -83,7 +83,6 @@
   environment.systemPackages = with pkgs; [
     vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
     htop
-    lm_sensors
     nix-tree
     ventoy-bin
     p7zip
