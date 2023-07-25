cce276213b8be009fcf0c3893c733cbdc6a794a1
Serg <reznov90210@gmail.com>
2023-07-25 21:54:13 +0300

    Rename default.nix to xdg.nix

--- home-manager/xdg.nix
+++ home-manager/xdg.nix
@@ -1,6 +1,4 @@
 {
-  imports = [ ./home.nix ];
-
   xdg = {
     userDirs = {
       enable = true;
--- hosts/nixos-host/home.nix
+++ hosts/nixos-host/home.nix
@@ -6,7 +6,10 @@
   home.username = "serg";
   home.homeDirectory = "/home/serg";
 
-  imports = [ ./programs ];
+  imports = [
+    ../..home-manager/xdg.nix
+    ../../home-manager/programs
+  ];
   home.stateVersion = "22.11"; # Please read the comment before changing.
 
   nixpkgs = { overlays = [ outputs.overlays.unstable-packages ]; };
@@ -31,22 +34,6 @@
     #If i need unstable: "unstable.nicotine"
     nicotine-plus
   ];
-
-  home.file = {
-    # # Building this configuration will create a copy of 'dotfiles/screenrc' in
-    # # the Nix store. Activating the configuration will then make '~/.screenrc' a
-    # # symlink to the Nix store copy.
-    # ".screenrc".source = dotfiles/screenrc;
-
-    # # You can also set the file content immediately.
-    # ".gradle/gradle.properties".text = ''
-    #   org.gradle.console=verbose
-    #   org.gradle.daemon.idletimeout=3600000
-    # '';
-  };
-  home.sessionVariables = {
-    # EDITOR = "emacs";
-  };
 
   # Let Home Manager install and manage itself.
   programs.home-manager.enable = true;
