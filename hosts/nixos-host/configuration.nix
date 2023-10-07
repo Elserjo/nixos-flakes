5d619cb7fbf9c78952e8e5feca13e7a7aed86051
Serg <reznov90210@gmail.com>
2023-10-07 20:49:27 +0300

    Remove gnome, set sway

--- hosts/nixos-host/configuration.nix
+++ hosts/nixos-host/configuration.nix
@@ -7,7 +7,8 @@
 {
   imports = [ # Include the results of the hardware scan.
     ./hardware-configuration.nix
-    ../../desktop/gnome.nix
+    # ../../desktop/gnome.nix
+    ../../desktop/sway.nix
     ../../users/backuppc.nix
     ../../services/sound.nix
     ../../services/syncthing.nix
