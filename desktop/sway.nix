11bf48b0b555d03ffaa7b2cc89e0a2e065e93f15
Serg <reznov90210@gmail.com>
2023-10-11 20:31:35 +0300

    Add swaykbdd package

--- desktop/sway.nix
+++ desktop/sway.nix
@@ -34,8 +34,11 @@
           "1" = [{ app_id = "firefox"; }];
           "2" = [{ app_id = "org.telegram.desktop"; }];
         };
-        startup =
-          [ { command = "firefox"; } { command = "telegram-desktop"; } ];
+        startup = [
+          { command = "firefox"; }
+          { command = "telegram-desktop"; }
+          { command = "${pkgs.swaykbdd}/bin/swaykbdd"; }
+        ];
         focus.followMouse = "no";
         modifier = "Mod4";
         menu = "${pkgs.wofi}/bin/wofi --show drun";
