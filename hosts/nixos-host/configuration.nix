8115f74de4b127d59ebeefade007773c0732456f
Serg <reznov90210@gmail.com>
2023-11-18 10:19:25 +0300

    Switch to gnome temporary?

--- hosts/nixos-host/configuration.nix
+++ hosts/nixos-host/configuration.nix
@@ -7,8 +7,8 @@
 {
   imports = [ # Include the results of the hardware scan.
     ./hardware-configuration.nix
-    # ../../desktop/gnome.nix
-    ../../desktop/sway.nix
+    ../../desktop/gnome.nix
+    # ../../desktop/sway.nix
     ../../users/backuppc.nix
     ../../services/sound.nix
     ../../services/syncthing.nix
