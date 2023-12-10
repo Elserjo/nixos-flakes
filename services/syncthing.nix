31db3ca41cd80d0af0eed026efc127c12aad05f3
Serg <reznov90210@gmail.com>
2023-12-10 16:28:04 +0300

    Change old syncthing options to new for 24.05

--- services/syncthing.nix
+++ services/syncthing.nix
@@ -7,20 +7,24 @@
     configDir = "/home/serg/.config/syncthing";
     overrideDevices = true;
     overrideFolders = true;
-    devices = {
-      "synology" = {
-        id = "IQBNIEA-MZQZGBU-YM2KVJE-B6D3QAV-UIXVCSU-OD5MIRY-E3UMC25-4ROKIAF";
+
+    settings = {
+      devices = {
+        "synology" = {
+          id =
+            "IQBNIEA-MZQZGBU-YM2KVJE-B6D3QAV-UIXVCSU-OD5MIRY-E3UMC25-4ROKIAF";
+        };
       };
-    };
-    folders."syncthing" = {
-      path = "/data/Media/syncthing";
-      devices = [ "synology" ];
-      versioning = {
-        type = "simple";
-        params = {
-          keep = "5";
-          cleanoutDays = "30";
-          cleanInterval = "3600";
+      folders."syncthing" = {
+        path = "/data/Media/syncthing";
+        devices = [ "synology" ];
+        versioning = {
+          type = "simple";
+          params = {
+            keep = "5";
+            cleanoutDays = "30";
+            cleanInterval = "3600";
+          };
         };
       };
     };
