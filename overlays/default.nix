bbb4bfc18fd0a21f0ccadb0cea7435b4f4767ba3
Serg <reznov90210@gmail.com>
2023-12-23 13:06:27 +0300

    Add example of pinning specific package version in overlay

--- overlays/default.nix
+++ overlays/default.nix
@@ -2,10 +2,17 @@
 
 {
   modifications = final: prev: {
-    tdesktop-no-ads = prev.tdesktop.overrideAttrs (old: {
-      patches = old.patches ++ [
-        ../patches/tdesktop-no-ads.patch
-      ];
-    });
+    tdesktop-no-ads = prev.tdesktop.overrideAttrs
+      (old: { patches = old.patches ++ [ ../patches/tdesktop-no-ads.patch ]; });
+
+    # Just an example how to pin specific package version
+    # picard = prev.picard.overrideAttrs (_: super: {
+    #   src = final.fetchFromGitHub {
+    #     owner = "metabrainz";
+    #     repo = "picard";
+    #     rev = "2.8.4";
+    #     hash = "sha256-ygZkj7hZNm7XyqDEI7l49d36ZgCTwFiAuYZjlF9d5+8=";
+    #   };
+    # });
   };
 }
