aa39cb05e85a2bb2c2189402dc4986b244b82bc9
Serg <reznov90210@gmail.com>
2024-01-29 11:18:44 +0300

    Add chromium package

--- home-manager/serg.nix
+++ home-manager/serg.nix
@@ -7,7 +7,7 @@
   home.homeDirectory = "/home/serg";
 
   imports =
-    [ ./xdg.nix ./programs/firefox.nix ./programs/git.nix ./programs/direnv.nix];
+    [ ./xdg.nix ./programs/firefox.nix ./programs/chromium ./programs/git.nix ./programs/direnv.nix];
   home.stateVersion = "22.11"; # Please read the comment before changing.
 
   nixpkgs = { overlays = [ outputs.overlays.modifications ]; };
