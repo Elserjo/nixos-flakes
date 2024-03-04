0eb296a73c6758bc69e7cf15ecdbf073d58111f0
Serg <reznov90210@gmail.com>
2024-03-04 19:17:39 +0300

    Use flacon from upstream (it was fixed)

--- overlays/default.nix
+++ overlays/default.nix
@@ -5,10 +5,6 @@
     tdesktop-no-ads = prev.tdesktop.overrideAttrs
       (old: { patches = old.patches ++ [ ../patches/tdesktop-no-ads.patch ]; });
 
-    # https://elatov.github.io/2022/01/building-a-nix-package/
-    # For qt libs we should append libsForQt5 before callPackage
-    flacon-with-ape = prev.libsForQt5.callPackage ../packages/flacon.nix { };
-
     cmus = prev.cmus.overrideAttrs (old: {
       src = final.fetchFromGitHub {
         owner = "cmus";
@@ -19,15 +15,5 @@
       # We don't need patches anymore
       patches = [ ];
     });
-
-    # Just an example how to pin specific package version
-    # picard = prev.picard.overrideAttrs (_: super: {
-    #   src = final.fetchFromGitHub {
-    #     owner = "metabrainz";
-    #     repo = "picard";
-    #     rev = "2.8.4";
-    #     hash = "sha256-ygZkj7hZNm7XyqDEI7l49d36ZgCTwFiAuYZjlF9d5+8=";
-    #   };
-    # });
   };
 }
