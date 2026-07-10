1b6c8ff97a5b6963327008b876d7b614365c3483
Serg <reznov90210@gmail.com>
2026-07-10 21:59:40 +0300

    First try to use darwin

--- flake.nix
+++ flake.nix
@@ -11,7 +11,7 @@
       type = "github";
       owner = "NixOS";
       repo = "nixpkgs";
-      ref = "release-24.05";
+      ref = "release-26.05";
     };
 
     home-manager = {
@@ -22,49 +22,32 @@
       inputs = { nixpkgs.follows = "nixpkgs"; };
     };
 
-    arkenfox-nixos = {
+    flake-parts = {
       type = "github";
-      owner = "dwarfmaster";
-      repo = "arkenfox-nixos";
-      ref = "main";
-      inputs = { nixpkgs.follows = "nixpkgs"; };
+      owner = "hercules-ci";
+      repo = "flake-parts";
+      inputs = {nixpkgs.follows = "nixpkgs"; };
+    };
+
+    nix-darwin = {
+      type = "github";
+      owner = "nix-darwin";
+      repo = "nix-darwin";
+      ref = "nix-darwin-26.05";
+      inputs = {nixpkgs.follows = "nixpkgs"; };
     };
   };
 
-  outputs = { self, nixpkgs, home-manager, ... }@inputs:
-    let
-      inherit (self) outputs;
-      system = "x86_64-linux";
-      commonModules = [ ./modules/common ] ++ [
-        home-manager.nixosModules.home-manager
-        {
-          #Need for overlays and for allow unfree packages
-          #inside homemanager module
-          home-manager.useGlobalPkgs = false;
-          home-manager.useUserPackages = true;
-          home-manager.extraSpecialArgs = { inherit inputs outputs; };
-          home-manager.sharedModules = [ ./modules/common ];
-        }
+  outputs = 
+    { ... }@inputs:
+    inputs.flake-parts.lib.mkFlake { inherit inputs; } {
+      systems = [
+        "x86_64-linux"
+        "aarch64-darwin"
       ];
-    in
-    {
-      overlays = import ./overlays { inherit inputs; };
-      # formatter.x86_64-linux = nixpkgs.legacyPackages.x86_64-linux.nixpkgs-fmt;
-      nixosConfigurations.nixos = nixpkgs.lib.nixosSystem {
-        inherit system;
-        modules = [ ./hosts/nixos-host/configuration.nix ] ++ commonModules;
-      };
-      homeConfigurations.opensuse-pc =
-        home-manager.lib.homeManagerConfiguration {
-          pkgs = import nixpkgs { system = "${system}"; };
-          extraSpecialArgs = { inherit inputs outputs; };
-          modules = [ ./hosts/opensuse-pc/serg.nix ];
-        };
-      homeConfigurations.arch-pc =
-        home-manager.lib.homeManagerConfiguration {
-          pkgs = import nixpkgs { system = "${system}"; };
-          extraSpecialArgs = { inherit inputs outputs; };
-          modules = [ ./hosts/arch-pc/serg.nix ];
-        };
-    };
+      imports = [
+        #./machines/amd64
+        ./machines/darwin
+      ];
+   };
 }
---
+++ machines/darwin/default.nix
@@ -0,0 +1,39 @@
+{ inputs, pkgs, lib, self, ... }:
+
+let 
+    homeConfig =  {
+        home-manager.users."serg" = {
+            home.homeDirectory = "/Users/serg";
+            home.stateVersion = "26.05";
+            imports = [
+            ../../home-manager/programs/git.nix
+            ../../home-manager/programs/vim.nix
+            ];
+        };
+        home-manager.useGlobalPkgs = true;
+        home-manager.useUserPackages = true;
+        home-manager.backupFileExtension = "bak";
+    };
+in
+{
+    flake = {
+        darwinConfigurations."Air-Sergej" = 
+            inputs.nix-darwin.lib.darwinSystem {
+                specialArgs = { inherit inputs; };
+                modules = [
+                    inputs.home-manager.darwinModules.home-manager
+                    homeConfig
+                    { nixpkgs.hostPlatform = "aarch64-darwin"; }
+                    ( { pkgs, ...}: {
+                        system.stateVersion = 4;
+                        nix.enable = false;
+                        home-manager.users.serg.home.homeDirectory = self.inputs.nixpkgs.lib.mkForce "/Users/serg";
+                        environment.systemPackages = [
+                            pkgs.hello
+                        ];
+                    })
+                ];
+            };
+        };
+}
+
